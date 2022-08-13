9ecca69fdc38db7099a425db2d2dba2e9753e53c
jeyakrrish <jeya.seelan1998@gmail.com>
2022-08-13 23:23:53 +0530

    added categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const categories = [
+  {
+    id: 1,
+    title: 'Hats',
+  },
+  {
+    id: 2,
+    title: 'Jackets',
+  },
+  {
+    id: 3,
+    title: 'Sneakers',
+  },
+  {
+    id: 4,
+    title: 'Women',
+  },
+  {
+    id: 5,
+    title: 'Men',
+  }
+];
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='categories-container'>
+      {categories.map(({ title, id }) => (            //destructuring
+        <div className='category-container' key={id}>
+          <img src='#' alt='shopping img' />
+          <div className='category-body-container'>
+            <h3>{title}</h3>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
