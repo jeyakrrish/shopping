28936425b532c1dba4ec61263180cbb99e5198b3
jeyakrrish <jeya.seelan1998@gmail.com>
2022-08-25 17:31:37 +0530

    Redux store initialized

--- src/index.js
+++ src/index.js
@@ -8,21 +8,26 @@ import { UserProvider } from './context/user-context.';
 import { CategoryContextProvider } from './context/category-context';
 import { CartContextProvider } from './context/cart-context';
 
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 import './index.scss';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoryContextProvider>
-          <CartContextProvider>
-            <App />
-          </CartContextProvider>
-        </CategoryContextProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider value={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoryContextProvider>
+            <CartContextProvider>
+              <App />
+            </CartContextProvider>
+          </CategoryContextProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
